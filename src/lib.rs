b9d62d9e0296eb260b130822d24b5af40530c0b1
Gabriel Ami <gabriel@simpleg.eu>
2022-10-15 01:44:34 +0300

    + git downloader functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod clone;
+pub mod downloader;
+pub mod error;
+pub mod git_config;
+pub mod pull;
+pub mod version_detector;
