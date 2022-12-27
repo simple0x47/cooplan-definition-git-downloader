2721b9541a0138b4dcaf609bdabbdb1544202f12
Gabriel Ami <gabriel@simpleg.eu>
2022-12-27 03:15:23 +0200

    + ability to set to latest version

--- src/downloader.rs
+++ src/downloader.rs
@@ -7,6 +7,8 @@ use crate::{
     pull::git_pull,
 };
 
+const MAIN_BRANCH: &str = "main";
+
 pub struct Downloader {
     repository_url: String,
     repository_local_dir: String,
@@ -69,27 +71,93 @@ impl Downloader {
     }
 
     pub fn set_version(&self, hash: String) -> Result<(), Error> {
-        match self.get_repository() {
-            Ok(repository) => match Oid::from_str(hash.as_str()) {
-                Ok(version) => match repository.set_head_detached(version) {
-                    Ok(_) => match repository.checkout_head(None) {
-                        Ok(_) => Ok(()),
-                        Err(error) => Err(Error::new(
-                            ErrorKind::VersionSetFailure,
-                            format!("failed to set version: {}", error).as_str(),
-                        )),
-                    },
-                    Err(error) => Err(Error::new(
-                        ErrorKind::VersionSetFailure,
-                        format!("failed to set version: {}", error).as_str(),
-                    )),
-                },
-                Err(error) => Err(Error::new(
+        let repository = match self.get_repository() {
+            Ok(repository) => repository,
+            Err(error) => return Err(error),
+        };
+
+        let version = match Oid::from_str(hash.as_str()) {
+            Ok(version) => version,
+            Err(error) => {
+                return Err(Error::new(
                     ErrorKind::VersionSetFailure,
                     format!("failed to set version: {}", error).as_str(),
-                )),
-            },
-            Err(error) => Err(error),
+                ))
+            }
+        };
+
+        match repository.set_head_detached(version) {
+            Ok(_) => (),
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::VersionSetFailure,
+                    format!("failed to set version: {}", error).as_str(),
+                ))
+            }
         }
+
+        match repository.checkout_head(None) {
+            Ok(_) => (),
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::VersionSetFailure,
+                    format!("failed to set version: {}", error).as_str(),
+                ))
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn set_version_to_latest(&self) -> Result<(), Error> {
+        let repository = self.get_repository()?;
+
+        let (_, reference) = match repository.revparse_ext(MAIN_BRANCH) {
+            Ok(result) => match result.1 {
+                Some(reference) => (result.0, reference),
+                None => {
+                    return Err(Error::new(
+                        ErrorKind::VersionSetFailure,
+                        "found no reference for main branch",
+                    ))
+                }
+            },
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::VersionSetFailure,
+                    format!("failed to set version: {}", error),
+                ))
+            }
+        };
+
+        match reference.name() {
+            Some(name) => match repository.set_head(name) {
+                Ok(_) => (),
+                Err(error) => {
+                    return Err(Error::new(
+                        ErrorKind::VersionSetFailure,
+                        format!("failed to set head: {}", error),
+                    ))
+                }
+            },
+            None => {
+                return Err(Error::new(
+                    ErrorKind::VersionSetFailure,
+                    "found no name for main branch reference",
+                ))
+            }
+        }
+
+        match repository.checkout_head(None) {
+            Ok(_) => (),
+            Err(error) => {
+                return Err(Error::new(
+                    ErrorKind::VersionSetFailure,
+                    format!("failed to checkout head: {}", error),
+                ))
+            }
+        }
+
+        Ok(())
     }
 }
